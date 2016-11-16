205e5acb1f69a6341f1a735103531816f1e9106f
Antonio Valle <vallegalvez_93@hotmail.com>
2016-11-15 18:55:34 -0700

    Calendario

--- app/src/main/java/com/yozzibeens/gobus/app/MainActivity.java
+++ app/src/main/java/com/yozzibeens/gobus/app/MainActivity.java
@@ -1,21 +1,36 @@
 package com.yozzibeens.gobus.app;
 
+import android.app.usage.UsageEvents;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 import com.yozzibeens.gobus.R;
+import com.yozzibeens.gobus.fragmentos.DateDialog;
 import com.yozzibeens.gobus.fragmentos.DrawerMenu;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerMenu mDrawerMenu;
 
-    LinearLayout boton;
+    LinearLayout calendarButton;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +44,34 @@ public class MainActivity extends AppCompatActivity {
         mDrawerMenu = (DrawerMenu) getSupportFragmentManager().findFragmentById(R.id.left_drawer);
         mDrawerMenu.setUp(R.id.left_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar, getSupportActionBar(), this);
 
-
-        boton = (LinearLayout) findViewById(R.id.boton);
-        boton.setOnClickListener(new View.OnClickListener()
-        {
-
+        calendarButton=(LinearLayout) findViewById(R.id.calendarButton);
+        calendarButton.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
-            public void onClick(View v) {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    DateDialog dialog=new DateDialog(v);
+                    android.app.FragmentTransaction ft=getFragmentManager().beginTransaction();
+                    dialog.show(ft,"DatePicker");
+                }
 
             }
         });
+        
     }
+
+    /*public void onStart(){
+        super.onStart();
+        calendarButton=(LinearLayout) findViewById(R.id.calendarButton);
+        calendarButton.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    DateDialog dialog=new DateDialog(v);
+                    android.app.FragmentTransaction ft=getFragmentManager().beginTransaction();
+                    dialog.show(ft,"DatePicker");
+                }
+
+            }
+        });
+    }*/
 }
