5b2c27816b75e28687fed0aca5c930f9495fb1cd
Antonio Valle <vallegalvez_93@hotmail.com>
2016-12-01 22:12:41 -0700

    Floating button y main casi terminado

--- app/src/main/java/com/yozzibeens/gobus/app/MainActivity.java
+++ app/src/main/java/com/yozzibeens/gobus/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements
     private LinearLayout calendarButton, hourButton;
     private TextView txtDate, txtHour;
     private FloatingActionButton btnBuscar;
+    private ImageView imgCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +105,9 @@ public class MainActivity extends AppCompatActivity implements
 
             }
         });
+
+        imgCity = (ImageView) findViewById(R.id.imgCity);
+        imgCity.setAlpha(35);
 
         }
 
